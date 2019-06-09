2d45b8cdfadbd0b90ba670fe94003f739dc0f9a3
Jacob Robinson <95jacob07@gmail.com>
2019-06-09 16:12:17 -0700

    Dark overlay added

--- src/components/CoverPage/index.js
+++ src/components/CoverPage/index.js
@@ -4,15 +4,18 @@ import React from "react";
 const CoverPage = props => {
   return (
     <div className="page cover-page text-center">
-      <div className="cover-page__content">
-        <div className="cover-page__title-0">
-          <i>Welcome to</i>
+      <div className="cover-page__overlay">
+        <div className="cover-page__content">
+          <div className="cover-page__title-0">
+            <i>Welcome to</i>
+          </div>
+          <div className="cover-page__title-1">Cutter Construction</div>
+          <p>
+            For a new design or replacing your old deck. We offer a
+            comprehensive solution using modern synthetic materials as well as
+            wood.
+          </p>
         </div>
-        <div className="cover-page__title-1">Cutter Construction</div>
-        <p>
-          For a new design or replacing your old deck. We offer a comprehensive
-          solution using modern synthetic materials as well as wood.
-        </p>
       </div>
     </div>
   );
