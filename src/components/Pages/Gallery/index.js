35c13979fe56b679d90b0c3824ba59e9de45a6e6
Jacob Robinson <95jacob07@gmail.com>
2019-06-11 18:04:23 -0700

    changed gallery photo

--- src/components/Pages/Gallery/index.js
+++ src/components/Pages/Gallery/index.js
@@ -6,7 +6,7 @@ import Banner from "../../Banner";
 
 const imageCSS = {
   background:
-    "url(https://images.unsplash.com/photo-1477058267562-6f70f44288a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)",
+    "url(https://images.unsplash.com/photo-1519970163512-f2d878f658c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)",
   backgroundPosition: "bottom"
 };
 
