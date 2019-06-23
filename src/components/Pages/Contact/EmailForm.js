d53544924c7bf1cb5014bfb7261363bfbbdd2aee
Jacob Robinson <95jacob07@gmail.com>
2019-06-22 22:29:17 -0700

    assigning errors

--- src/components/Pages/Contact/EmailForm.js
+++ src/components/Pages/Contact/EmailForm.js
@@ -7,8 +7,8 @@ const EmailForm = props => {
   const onSubmit = async items => {
     try {
       const result = await axios.post(
-        // "http://localhost:9000/.netlify/functions/api/cutter-construction/email", // dev
-        "https://robinson-endpoints.netlify.com/.netlify/functions/api/cutter-construction/email", // prod
+        "http://localhost:9000/.netlify/functions/api/cutter-construction/email", // dev
+        // "https://robinson-endpoints.netlify.com/.netlify/functions/api/cutter-construction/email", // prod
         { ...items }
       );
       console.log(result);
@@ -17,18 +17,43 @@ const EmailForm = props => {
     }
   };
 
-  const validate = values => {};
+  const validate = values => {
+    const errors = {};
+
+    if (!values.name) {
+      errors.name = "Required";
+    }
+    if (!values.email) {
+      errors.email = "Required";
+    }
+    if (!values.phone) {
+      errors.phone = "Required";
+    }
+    if (!values.subject) {
+      errors.subject = "Required";
+    }
+    if (!values.message) {
+      errors.message = "Required";
+    }
+
+    return errors;
+  };
+
+  const hasError = bool => {
+    console.log(bool);
+    return bool ? "form-control error" : "form-control";
+  };
 
   return (
     <Form
       onSubmit={onSubmit}
       validate={validate}
-      render={({ handleSubmit, pristine, invalid }) => (
+      render={({ handleSubmit, pristine, invalid, errors }) => (
         <form className="contact__form" onSubmit={handleSubmit}>
           <div className="form-group">
             <Field
               component="input"
-              className="form-control"
+              className={hasError(errors.name)}
               type="text"
               name="name"
               placeholder="Name"
@@ -37,7 +62,7 @@ const EmailForm = props => {
           <div className="form-group">
             <Field
               component="input"
-              className="form-control"
+              className={hasError(errors.email)}
               type="email"
               name="email"
               placeholder="Email"
@@ -47,7 +72,7 @@ const EmailForm = props => {
             <Field
               component="input"
               name="phone"
-              className="form-control"
+              className={hasError(errors.phone)}
               type="text"
               placeholder="Phone"
             />
@@ -56,7 +81,7 @@ const EmailForm = props => {
             <Field
               component="input"
               name="subject"
-              className="form-control"
+              className={hasError(errors.subjec)}
               type="text"
               placeholder="Subject"
             />
@@ -65,7 +90,7 @@ const EmailForm = props => {
             <Field
               component="textarea"
               name="message"
-              className="form-control"
+              className={hasError(errors.message)}
               type="text"
               placeholder="Message"
               rows="6"
