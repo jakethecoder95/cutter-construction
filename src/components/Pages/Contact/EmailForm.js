cd6cd32c93cb53d21fc3e8b1ff632d9f5fa33b7c
Jacob Robinson <95jacob07@gmail.com>
2019-06-28 10:15:07 -0700

    Handling email submit

--- src/components/Pages/Contact/EmailForm.js
+++ src/components/Pages/Contact/EmailForm.js
@@ -1,5 +1,5 @@
 import "./EmailForm.scss";
-import React from "react";
+import React, { useState } from "react";
 import { Form, Field } from "react-final-form";
 import axios from "axios";
 
@@ -7,17 +7,24 @@ import ContactField from "./ContactField";
 import isEmail from "./util/regexEmail";
 
 const EmailForm = props => {
+  const [submitting, setSubmitting] = useState(false);
+  const [emailResponse, setEmailResponse] = useState("");
+
   const onSubmit = async items => {
+    setSubmitting(true);
     try {
-      const result = await axios.post(
+      const response = await axios.post(
         // "http://localhost:9000/.netlify/functions/api/cutter-construction/email", // dev
         "https://robinson-endpoints.netlify.com/.netlify/functions/api/cutter-construction/email", // prod
         { ...items }
       );
-      console.log(result);
+      setEmailResponse("success");
+      console.log(response);
     } catch (error) {
-      console.log(error.result);
+      setEmailResponse("error");
+      console.log(error.response);
     }
+    setSubmitting(false);
   };
 
   const validate = values => {
@@ -52,8 +59,14 @@ const EmailForm = props => {
     <Form
       onSubmit={onSubmit}
       validate={validate}
-      render={({ handleSubmit, pristine, invalid, errors }) => (
-        <form className="contact__form" onSubmit={handleSubmit}>
+      render={({ handleSubmit, form }) => (
+        <form
+          className="contact__form"
+          onSubmit={async e => {
+            await handleSubmit(e);
+            form.reset();
+          }}
+        >
           <div className="form-group">
             <Field component={ContactField} name="name" />
           </div>
@@ -69,7 +82,31 @@ const EmailForm = props => {
           <div className="form-group">
             <Field component={ContactField} name="message" />
           </div>
-          <button type="submit">SEND</button>
+          <button type="submit" disabled={submitting}>
+            {(submitting && (
+              <div>
+                <span
+                  className="spinner-grow spinner-grow-sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+                SENDING...
+              </div>
+            )) || <span>SEND</span>}
+          </button>
+          {emailResponse === "success" && (
+            <div className="alert alert-success" role="alert">
+              Success! Your email was sent. We will get back to you soon.
+            </div>
+          )}
+          {emailResponse === "error" && (
+            <div className="alert alert-error" role="alert">
+              Error: There was an issue there.{" "}
+              <a href="mailto:cuttercon11@gmail.com">
+                Try manually emailing us.
+              </a>
+            </div>
+          )}
         </form>
       )}
     />
