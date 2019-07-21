831d7655ae5123df9047de25658eba316333eece
Jacob Robinson <95jacob07@gmail.com>
2019-07-20 21:51:53 -0700

    about page done

--- src/components/Pages/About/AboutContent.js
+++ src/components/Pages/About/AboutContent.js
@@ -8,31 +8,29 @@ const AboutContent = props => {
           <div className="item col-md-3">
             <h3>EXCELLENCE AND PROFESSIONALISM</h3>
             <p>
-              Here is an example paragraph expanding on the header. You can
-              decide whatever you want to say here. For example, "To us,
-              building is not just a job it is who we are and we cannot imagine
-              doing anything else". For consistency please try to make it as
-              long as this :)
+              Cutter Construction has been building relationships and trust with
+              our customers since 2011 in Lake of the Pines, Grass Valley, and
+              Auburn.We are an active remodeling and deck building business and
+              believe that no project is too big or small for our team.
             </p>
           </div>
           <div className="item col-md-3 col-md-offset-3">
             <h3>PRIDE OF CRAFTMENSHIP</h3>
             <p>
-              Here is an example paragraph expanding on the header. You can
-              decide whatever you want to say here. For example, "To us,
-              building is not just a job it is who we are and we cannot imagine
-              doing anything else". For consistency please try to make it as
-              long as this :)
+              We believe rewarding projects are possible only through a clear
+              vision and great design. We’re there to assist at every step of
+              the process as we realize the potential of your home. By using
+              quality materials and creating efficient use of spaces, we provide
+              value and enjoyment for years to come!
             </p>
           </div>
           <div className="item col-md-3">
-            <h3>EXAMPLE NUMBER THREE</h3>
+            <h3>Lets Connect!</h3>
             <p>
-              Here is an example paragraph expanding on the header. You can
-              decide whatever you want to say here. For example, "To us,
-              building is not just a job it is who we are and we cannot imagine
-              doing anything else". For consistency please try to make it as
-              long as this :)
+              Thank you so much for visiting our site and taking the time to
+              learn about our services. We appreciate your interest and aspire
+              to hearing more about your remodeling ideas. Give us a call at
+              (530)401-5084.
             </p>
           </div>
         </div>
